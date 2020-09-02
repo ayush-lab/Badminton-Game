7ea129ac124b3455648ccb0e975081ea3a0e770c
Kapil <kapil1910086@akgec.ac.in>
2020-09-02 12:55:25 +0530

    Added Player with movement and Constraint

--- js/game.js
+++ js/game.js
@@ -1,179 +1,244 @@
 var canvas = document.getElementById('game');
- var ctx = canvas.getContext('2d');
- var height=canvas.height/1.2;
- var flag=0;
- var x_pos=180;
- var y_pos=height-1;
- var gr=0;
- var angle=(45*(Math.PI)/180);
- var requestAnimationFrame  = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitCancelAnimationFrame;   
- var cancelAnimationFrame   = window.cancelAnimationFrame  || window.mozCancelAnimationFrame;    
- var animationstart;
- var img = new Image();   
- 
- //AUDIO FILES
-var audioElement= new Audio('assests/introsong.mp3');
+var ctx = canvas.getContext('2d');
+var height = canvas.height / 1.2;
+var flag = 0;
+var x_pos = 180;
+var y_pos = height - 1;
+var gr = 0;
+var angle = (45 * (Math.PI) / 180);
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitCancelAnimationFrame;
+var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+var animationstart;
+var img = new Image();
+var leftplayer = new Image();
+var rightplayer = new Image();
+var a_pressed = false;
+var d_pressed = false;
+var left_pressed = false;
+var right_pressed = false;
+var left_player_x = 120;
+var right_player_x = 1000;
+var left_player_y = 600;
+var right_player_y = 600;
+var leftscore = 0;
+var rightscore = 0;
+//AUDIO FILES
+var audioElement = new Audio('assests/introsong.mp3');
 
 
-audioElement.currentTime=0;
+audioElement.currentTime = 0;
 audioElement.pause();
 
 img.src = '../Images/shuttle.svg';
+leftplayer.src = '../Images/left.png';
+rightplayer.src = '../Images/right.png'
 
 
 document.addEventListener("keydown", keydownF, false);
+document.addEventListener("keyup", keyUpHandler, false);
 //document.addEventListener('keydown', clear_shot_forward);
 document.addEventListener('mousedown', mouseact, false);
 
 
-function mouseact(ob){
+function mouseact(ob) {
     //alert(ob.clientX, " , ", ob.screenX, ',', ob.pageX);
-    alert(ob.clientX -75);
+    alert(ob.clientX - 75);
 }
 
 animationstart = requestAnimationFrame(draw);
 
-var h_vel=5.5;
-var v_vel=5.5;
-var g=0.07;
+var h_vel = 5.5;
+var v_vel = 5.5;
+var g = 0.07;
 
- function draw() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.drawImage(img,x_pos,y_pos,30,30);
-
-    if(y_pos >= height || x_pos<180 || x_pos >1160){
-        cancelAnimationFrame(animationstart);
-    }
-
-
-    else    animationstart = requestAnimationFrame(draw);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, x_pos, y_pos, 30, 30);
+    LeftPlayer();
+    RightPlayer();
+    //  if(y_pos >= height || x_pos<180 || x_pos >1160){
+    //      cancelAnimationFrame(animationstart);
+    //  }
 
 
+    //  else    
+    animationstart = requestAnimationFrame(draw);
 
-    if(flag==0){ 
+    if (flag == 0) {
         forward();
         clear_shot_f();
     }
 
-    else if(flag==1) {
+    else if (flag == 1) {
         backward();
         clear_shot_b();
     }
-
-
-    else if(flag==2){
+    else if (flag == 2) {
         forward();
         drop_shot_f_f();
         console.log("forward ->", gr);
-    
+
     }
-    
-    else if(flag==3){
+
+    else if (flag == 3) {
         backward();
         drop_shot_f_b();
         console.log("backward->", gr);
-    
+
     }
-    else if(flag==4){
+    else if (flag == 4) {
         forward();
         drop_shot_b_f();
 
     }
-    else if(flag==5){
+    else if (flag == 5) {
         backward();
         drop_shot_b_b();
-        
-    }
-  
- 
-  
-}
 
+    }
+
+
+
+}
 //  CONTROLS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-function keydownF(ob){
-    
-    if(ob.key ==='e'){
-        flag=0;
-        gr=0;
-
+function keydownF(ob) {
+    if (ob.key == "a") {
+        a_pressed = true;
     }
-    
-    if(ob.key==='4'){
-        flag=1;
-        gr=0;
-  
+    if (ob.key == "d") {
+        d_pressed = true;
     }
-
-
-    if(ob.key === 'r'){
-        flag=2;
-        gr=0;
+    if (ob.key == "ArrowLeft") {
+        left_pressed = true;
+    }
+    if (ob.key == "ArrowRight") {
+        right_pressed = true;
     }
 
-    if(ob.key === '5'){
-        flag=3;
-        gr=0;
+    if (ob.key === 'e') {
+        flag = 0;
+        gr = 0;
+
+    }
+
+    if (ob.key === '4') {
+        flag = 1;
+        gr = 0;
+
     }
 
 
-    if(ob.key === 'y'){
-        flag=4;
-        gr=0;
+    if (ob.key === 'r') {
+        flag = 2;
+        gr = 0;
     }
 
-    if(ob.key === '+'){
-        flag=5;
-        gr=0;
+    if (ob.key === '5') {
+        flag = 3;
+        gr = 0;
     }
 
+
+    if (ob.key === 'y') {
+        flag = 4;
+        gr = 0;
+    }
+
+    if (ob.key === '+') {
+        flag = 5;
+        gr = 0;
+    }
+
+
+}
+function keyUpHandler(ob) {
+    if (ob.key == "a") {
+        a_pressed = false;
+    }
+    if (ob.key == "d") {
+        d_pressed = false;
+    }
+    if (ob.key == "ArrowLeft") {
+        left_pressed = false;
+    }
+    if (ob.key == "ArrowRight") {
+        right_pressed = false;
+    }
 
 }
 
 //////////////////////////////////          END OF CONTROLS          ///////////////////////////////////////////////// 
-
-
- function forward(){
-    x_pos+=h_vel;
-    y_pos-=v_vel;
-    y_pos+=gr;
-   // console.log(y_pos, "->" ,x_pos);
-}
-
- function backward(){
-  
-    x_pos-=h_vel;
-    y_pos-=v_vel;
-    y_pos+=gr;
-   // console.log('okkk');
+function LeftPlayer() {
+    ctx.beginPath();
+    ctx.drawImage(leftplayer, left_player_x, left_player_y, 120, 120);
+    ctx.closePath();
+    if (d_pressed && left_player_x < 580) {
+        left_player_x += 6;
+    }
+    if (a_pressed && left_player_x >= 100) {
+        left_player_x += -6;
     }
 
-
-
-function clear_shot_f(){
-    gr+=0.07;
 }
+function RightPlayer() {
+    ctx.beginPath();
+    ctx.drawImage(rightplayer, right_player_x, right_player_y, 120, 120);
+    ctx.closePath();
+    if (left_pressed && right_player_x > 710) {
+        right_player_x += -6;
+    }
+    if (right_pressed && right_player_x <= 1100) {
+        right_player_x += 6;
+    }
 
-function clear_shot_b(){
-    gr+=0.07;
-}
-
-
-
-function drop_shot_f_f(){     //drop shot from front to front (forward)
-    gr+=0.2;
-}
-
-function drop_shot_f_b(){     //drop shot from front to front (backward)
-    gr+=0.15;
 }
 
 
-
-function drop_shot_b_f(){     //drop shot from back to front (forward)
-    gr+=0.15;
+function forward() {
+    x_pos += h_vel;
+    y_pos -= v_vel;
+    y_pos += gr;
+    // console.log(y_pos, "->" ,x_pos);
 }
 
-function drop_shot_b_b(){     //drop shot from back to front (backward)
-    gr+=0.1;
+function backward() {
+
+    x_pos -= h_vel;
+    y_pos -= v_vel;
+    y_pos += gr;
+    // console.log('okkk');
+}
+
+
+
+function clear_shot_f() {
+    gr += 0.07;
+}
+
+function clear_shot_b() {
+    gr += 0.07;
+}
+
+
+
+function drop_shot_f_f() {     //drop shot from front to front (forward)
+    gr += 0.2;
+}
+
+function drop_shot_f_b() {     //drop shot from front to front (backward)
+    gr += 0.15;
+}
+
+
+
+function drop_shot_b_f() {     //drop shot from back to front (forward)
+    gr += 0.15;
+}
+
+function drop_shot_b_b() {     //drop shot from back to front (backward)
+    gr += 0.1;
+}
+function leftplayer() {
+
 }
